8cfdee0e9293ea8f8d6eb186e0a412d3a1b966e8
nathanap3 <nathan.palluau@oso-ai.com>
2025-01-23 15:08:57 +0100

    add core files

---
+++ visualization.py
@@ -0,0 +1,7 @@
+def visualize_vector_field_quiver(field: np.ndarray):
+    _,H,W = field.shape
+    y_coords, x_coords = np.mgrid[0:H, 0:W]
+    dx, dy = field
+    plt.figure()
+    plt.quiver(x_coords,y_coords,dx,dy,scale_units='xy',color='blue')
+    plt.show() 
